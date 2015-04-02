d9324524c86f003f48bf74df62772983f5f666aa
jo <johannes.denninger@gmail.com>
2015-04-02 17:01:51 +0200

    sw

--- sw.py
+++ sw.py
@@ -3,7 +3,7 @@ import jinja2
 import os
 
 
-with open('rtr.yml', 'r') as f:
+with open('sw.yml', 'r') as f:
     i = yaml.load(f)
 
 
@@ -12,7 +12,7 @@ jenv = jinja2.Environment(
     trim_blocks=True, lstrip_blocks=True
 )
 
-template = jenv.get_template('rtr.j2')
+template = jenv.get_template('sw.j2')
 
 
 print template.render(item = i)
