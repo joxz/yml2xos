bde554259363b4a1a054afd8c9bdc5c3c1ba86c4
jo <johannes.denninger@gmail.com>
2015-04-02 12:23:57 +0200

    initial

---
+++ rtr.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+import yaml
+import jinja2
+import os
+
+with open('rtr.yml', 'r') as f:
+    i = yaml.load(f)
+
+
+jenv = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.getcwd()),
+    trim_blocks=True, lstrip_blocks=True
+)
+
+template = jenv.get_template('rtr.j2')
+
+
+
+print template.render(item = i)
